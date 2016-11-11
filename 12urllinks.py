8b1cf18a07f52a62e4d332acf579fba4332b6328
ferguszhang <q16399zy@gmail.com>
2016-11-11 12:29:38 +0800

    chapter12.4's exercise

--- 12urllinks.py
+++ 12urllinks.py
@@ -1,2 +1,18 @@
 import urllib
-import BeautifulSoup
+from bs4 import BeautifulSoup as BSoup
+
+while True:
+    inurl = raw_input("Please enter a url likes: 'http://www.dr-chuck.com/page1.htm' or over with 'done'.\n>:")
+    if inurl == "done":break
+    try:
+        uhand = urllib.urlopen(inurl).read()
+    except:
+        print "This enter is invalid!"
+        continue
+    soup = BSoup(uhand, "html.parser")
+
+    tags = soup('a')
+    print "\nThese are parsed result:"
+    for tag in tags:
+        print tag.get("href", None)
+    print "DONE\n\n"
